b3bb67afbade641bf25f4590fd60ba876ae6b45e
AnnaRemi <60964233+AnnaRemi@users.noreply.github.com>
2020-09-23 19:13:30 +0300

    Update C.cpp
    some small fixes

--- C.cpp
+++ C.cpp
@@ -1,49 +1,3 @@
-/*#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using std::cin;
-using std::cout;
-using std::vector;
-using std::string;
-using std::endl;
-using std::swap;
-
-class command{
-public:
-    string surname{};
-    string name{};
-    int avg{};
-};
-
-class sort_class{
-public:
-    bool operator()(const command& Obj1, const command& Obj2)const{
-        return Obj1.avg > Obj2.avg;
-    }
-};
-
-int main() {
-    int size, inf, maths, rus;
-    cin >> size;
-
-    vector<command> arr;
-    arr.reserve(size);
-
-    sort_class sort_object;
-
-    for (int i = 0; i < size; ++i){
-        command temp;
-        cin >> temp.surname >> temp.name >> inf >> maths >> rus;
-        temp.avg = inf + maths + rus;
-        arr.push_back(temp);
-    }
-
-    sort (arr.begin(), arr.end(), sort_object);
-
-    for (const auto & i : arr)
-        cout << i.surname << ' ' << i.name << endl;
-}*/
 
 #include <iostream>
 #include <vector>
@@ -57,21 +11,22 @@ using std::vector;
 typedef class Student {
 public:
     string surname, name;
-    int avg{};
+    int summ{};
 }Student;
 
+typedef vector<Student>::iterator Iter;
 
-void merge(vector<Student>::iterator a_beg, vector<Student>::iterator a_end,
-           vector<Student>::iterator b_beg, vector<Student>::iterator b_end) {
+void merge(Iter first_beg, Iter first_end,
+           Iter second_beg, Iter second_end) {
 
-    size_t c_size = a_end - a_beg + b_end - b_beg;
+    size_t c_size = first_end - first_beg + second_end - second_beg;
     vector<Student> merged;
     merged.reserve(c_size);
 
-    auto first_beg = a_beg;
-    auto second_beg = b_beg;
-    while (first_beg != a_end && second_beg != b_end) {
-        if (first_beg->avg >= second_beg->avg) {
+    auto first_beg = first_beg;
+    auto second_beg = second_beg;
+    while (first_beg != first_end && second_beg != second_end) {
+        if (first_beg->summ >= second_beg->summ) {
             merged.emplace_back(*first_beg);
             first_beg = next(first_beg);
         } else {
@@ -79,26 +34,26 @@ void merge(vector<Student>::iterator a_beg, vector<Student>::iterator a_end,
             second_beg = next(second_beg);
         }
     }
-    for (; first_beg != a_end; first_beg = next(first_beg))
+    for (; first_beg != first_end; first_beg = next(first_beg))
         merged.emplace_back(*first_beg);
 
-    for (; second_beg != b_end; second_beg = next(second_beg))
+    for (; second_beg != second_end; second_beg = next(second_beg))
         merged.emplace_back(*second_beg);
 
     auto m_beg = merged.begin();
 
-    for (first_beg = a_beg; first_beg != a_end; first_beg = next(first_beg)) {
+    for (first_beg = first_beg; first_beg != first_end; first_beg = next(first_beg)) {
         *first_beg = *m_beg;
         m_beg = next(m_beg);
     }
-    for (second_beg = b_beg; second_beg != b_end; second_beg = next(second_beg)) {
+    for (second_beg = second_beg; second_beg != second_end; second_beg = next(second_beg)) {
         *second_beg = *m_beg;
         m_beg = next(m_beg);
     }
 }
 
 void merge_sort(vector<Student>::iterator beg, vector<Student>::iterator end) {
-    int shift = distance(beg, end);
+    size_t shift = distance(beg, end);
     if (shift <= 1)
         return;
     auto mid = next(beg, shift / 2);
@@ -109,20 +64,21 @@ void merge_sort(vector<Student>::iterator beg, vector<Student>::iterator end) {
 
 
 int main() {
-    int size, inf, maths, rus;
+    size_t size;
+    int inf, maths, rus;
     cin >> size;
 
-    vector<Student> arr;
-    arr.reserve(size);
+    vector<Student> arr(size);
 
     for (int i = 0; i < size; ++i){
         Student temp;
         cin >> temp.surname >> temp.name >> inf >> maths >> rus;
-        temp.avg = inf + maths + rus;
+        temp.summ = inf + maths + rus;
         arr.push_back(temp);
     }
 
     merge_sort(arr.begin(), arr.end());
+
     for (const auto & i : arr)
         cout << i.surname << ' ' << i.name << std::endl;
 
